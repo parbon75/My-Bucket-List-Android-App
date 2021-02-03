dc94b7d95827bf106af01e695d2c03ce783b0938
Mehdad Hossain <parbon75@gmail.com>
2021-02-03 02:19:10 -0800

    First commit. Main activity layout is done

---
+++ app/src/main/java/com/parbon/mybucketlist/PlacesToGoActivity.java
@@ -0,0 +1,14 @@
+package com.parbon.mybucketlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlacesToGoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_to_go);
+    }
+}
